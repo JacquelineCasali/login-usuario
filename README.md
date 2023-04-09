6edf54c577f5abdd2fcc589d8c6e3a2b22a801ea
JacquelineCasali <jcasalia@yahoo.com.br>
2023-04-09 13:26:05 -0300

    vercel

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ JavaScript CSS EJS
 Imagem do projeto
 
 ![logint](https://user-images.githubusercontent.com/103325619/221649923-b87fb20b-61f0-40da-9783-7c668680d2d3.gif)
+
+<a href="https://formulario-vercel.vercel.app"></a>
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const port = 3000;
+
 const methodOverride = require("method-override");
 const path = require("path");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const indexRoute = require("./src/routes/indexRoute");
 const authRoute = require("./src/routes/authRoute");
+const port = 3000;
 
 // Configura o methodOverride no express
 // methodOverride = Pacote que transforma um método http em outro
@@ -34,3 +36,16 @@ app.use("/", authRoute);
 app.listen(port, () => {
   console.log("Estamos rodando em: http://localhost:" + port);
 });
+
+// const express = require("express");
+
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Olá Docker! Atualização ");
+// });
+
+// app.listen(port, () => {
+//   console.log(`App rodando na porta:${port}`);
+// });
